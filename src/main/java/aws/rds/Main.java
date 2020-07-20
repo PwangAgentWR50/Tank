037b896a206cce0480d48d39bd267ac7e55477f4
Peiding Wang <peidingw@amazon.com>
2020-07-19 23:15:17 -0700

    remove bullet memory leakage

---
+++ src/main/java/aws/rds/Main.java
@@ -0,0 +1,15 @@
+package aws.rds;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        TankFrame tf = new TankFrame();
+
+        while(true) {
+            Thread.sleep(50);
+            tf.repaint();
+        }
+
+    }
+
+}
