6d12cdc1398cfe77500038debbdf217c5234fde0
Peiding Wang <peidingw@amazon.com>
2020-07-20 18:29:57 -0700

    add 5 new tank

--- src/main/java/aws/rds/TankFrame.java
+++ src/main/java/aws/rds/TankFrame.java
@@ -13,8 +13,9 @@ import java.util.List;
 
 public class TankFrame extends Frame {
 
-    Tank myTank = new Tank(200, 200, Dir.DOWN, this);
+    Tank myTank = new Tank(200, 400, Dir.DOWN, this);
     List<Bullet> bullets = new ArrayList<>();
+    List<Tank> tanks = new ArrayList<>();
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
 
@@ -63,6 +64,9 @@ public class TankFrame extends Frame {
             bullets.get(i).paint(g);
         }
 
+        for (int i = 0; i < tanks.size(); i++) {
+            tanks.get(i).paint(g);
+        }
     }
 
 
