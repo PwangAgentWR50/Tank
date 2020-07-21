e0c57e7043150a4b39a6a17e380877d589b87bf8
Peiding Wang <peidingw@amazon.com>
2020-07-20 18:10:47 -0700

    replace bullet with image

--- src/main/java/aws/rds/Bullet.java
+++ src/main/java/aws/rds/Bullet.java
@@ -1,5 +1,4 @@
 package aws.rds;
-import java.awt.Color;
 import java.awt.Graphics;
 
 public class Bullet {
@@ -24,10 +23,20 @@ public class Bullet {
             tf.bullets.remove(this);
         }
 
-        Color c = g.getColor();
-        g.setColor(Color.RED);
-        g.fillOval(x, y, WIDTH, HEIGHT);
-        g.setColor(c);
+        switch (dir) {
+            case LEFT:
+                g.drawImage(ResourceMgr.bulletL, x, y, null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.bulletR, x, y, null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.bulletU, x, y, null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.bulletD, x, y, null);
+                break;
+        }
 
         move();
     }
